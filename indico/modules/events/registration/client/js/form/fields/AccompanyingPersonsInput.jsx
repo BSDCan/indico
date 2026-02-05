77efe15b322f3f0817d1aed9cbaee914dc3a6209
Graham Percival <gperciva@tarsnap.com>
2026-02-04 20:30:27 -0800

    Reception: disable cost-option; free until May 1

--- indico/modules/events/registration/client/js/form/fields/AccompanyingPersonsInput.jsx
+++ indico/modules/events/registration/client/js/form/fields/AccompanyingPersonsInput.jsx
@@ -72,7 +72,7 @@ const ReceptionOptions = [
   'No',
   // BSDCAN: only enable one 'Yes' at once (after testing is finished)
   'Yes (free until May 1)',
-  `Yes (cost $64)`,
+  // `Yes (cost $64)`,
   // Comment so that the linter allows this to be a list, not all one line.
 ];
 
